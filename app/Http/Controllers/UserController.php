6c4dec8abba9eb4c21b64e1083073d21703016f5
Susana Piñero <susananzth@gmail.com>
2021-12-28 21:07:48 -0500

    Actualizando controlador de usuario. Añadiendo mensajes de success y danger, try catch, etc.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use Illuminate\Support\Facades\Gate;
-use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
@@ -18,11 +17,14 @@ class UserController extends Controller
      */
     public function index()
     {
-        abort_if(Gate::denies('user_index'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        $users = User::with('roles')->get();
-
-        return view('users.index', compact('users'));
+        if (Gate::denies('user_index')) {
+            return redirect()->route('home')
+                ->with('message', 'No cuenta con los permisos necesarios para ejecutar la acción.')
+                ->with('alert_class', 'danger');
+        } else {
+            $users = User::with('roles')->get();
+            return view('users.index', compact('users'));
+        }
     }
 
     /**
@@ -32,84 +34,133 @@ class UserController extends Controller
      */
     public function create()
     {
-        abort_if(Gate::denies('user_add'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        $roles = Role::pluck('title', 'id');
-
-        return view('users.create', compact('roles'));
+        if (Gate::denies('user_add')) {
+            return redirect()->route('user.index')
+                ->with('message', 'No cuenta con los permisos necesarios para ejecutar la acción.')
+                ->with('alert_class', 'danger');
+        } else {
+            $roles = Role::all();
+            return view('users.create', compact('roles'));
+        }
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\StoreUserRequest  $request
      * @return \Illuminate\Http\Response
      */
     public function store(StoreUserRequest $request)
     {
-        $user = User::create($request->validated());
-        $user->roles()->sync($request->input('roles', []));
-
-        return redirect()->route('user.index');
+        $validated = $request->validated();
+        try {
+            $user = User::create($request->validated());
+            $user->roles()->sync($validated['role']);
+            return redirect()->route('user.index')
+                ->with('message', 'Usuario registrado con éxito.')
+                ->with('alert_class', 'success');
+        } catch (\Throwable $th) {
+            //throw $th;
+            return redirect()->back()->withInput()
+                ->with('message', 'Oops! Ha ocurrido un error.')
+                ->with('alert_class', 'danger');
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
     public function show(User $user)
     {
-        abort_if(Gate::denies('user_index'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        return view('users.show', compact('user'));
+        // Valida si tiene permiso de ver.
+        if (Gate::denies('user_index')) {
+            // Muestra mensaje en alert de la vista. Función en JS refresca navegador.
+            session()->flash('message', 'No cuenta con los permisos necesarios para ejecutar la acción.');
+            session()->flash('alert_class', 'danger');
+            return response()->json([
+                'code' => '403',
+                'status' => 'Forbidden.'
+            ]);
+        } else {
+            return response()->json([
+                'code' => '200',
+                'status' => 'Ok.',
+                'user' => $user,
+                'roles' => $user->roles,
+            ]);
+        }
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
     public function edit(User $user)
     {
-        abort_if(Gate::denies('user_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        $roles = Role::pluck('title', 'id');
-
-        $user->load('roles');
-
-        return view('users.edit', compact('user', 'roles'));
+        if (Gate::denies('user_edit')) {
+            return redirect()->route('user.index')
+                ->with('message', 'No cuenta con los permisos necesarios para ejecutar la acción.')
+                ->with('alert_class', 'danger');
+        } else {
+            $data['roles'] = Role::all();
+            $data['user'] = $user->load('roles');
+            return view('users.edit', $data);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\UpdateUserRequest  $request
+     * @param  \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
     public function update(UpdateUserRequest $request, User $user)
     {
-        $user->update($request->validated());
-        $user->roles()->sync($request->input('roles', []));
-
-        return redirect()->route('user.index');
+        $validated = $request->validated();
+        try {
+            $user->update($validated);
+            $user->roles()->sync($validated['role']);
+            return redirect()->back()
+                ->with('message', 'Usuario actualizado con éxito.')
+                ->with('alert_class', 'success');
+        } catch (\Throwable $th) {
+            //throw $th;
+            return redirect()->back()->withInput()
+                ->with('message', 'Oops! Ha ocurrido un error.')
+                ->with('alert_class', 'danger');
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Models\User $user
      * @return \Illuminate\Http\Response
      */
     public function destroy(User $user)
     {
-        abort_if(Gate::denies('user_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
-        $user->delete();
-
-        return redirect()->route('user.index');
+        if (Gate::denies('user_delete')) {
+            return redirect()->route('user.index')
+                ->with('message', 'No cuenta con los permisos necesarios para ejecutar la acción.')
+                ->with('alert_class', 'danger');
+        } else {
+            try {
+                $user->delete();
+                return redirect()->route('user.index')
+                    ->with('message', 'Usuario eliminado con éxito.')
+                    ->with('alert_class', 'success');
+            } catch (\Throwable $th) {
+                //throw $th;
+                return redirect()->route('user.index')
+                    ->with('message', 'Oops! Ha ocurrido un error.')
+                    ->with('alert_class', 'danger');
+            }
+        }
     }
 }
